74fe68822ba09bc083c697b422c0978b7462526a
003 Disegno <Dis003@stageo4.local>
2026-06-17 10:29:01 +0200

    Rimuove funzioni temporanee workflow

--- landing/urls.py
+++ landing/urls.py
@@ -4,8 +4,6 @@ from .views import (
     home,
     privacy_policy,
     termini_utilizzo,
-    carica_workflow_professionali,
-    pulisci_workflow_doppi,
 )
 
 
@@ -15,16 +13,4 @@ urlpatterns = [
     path('privacy/', privacy_policy, name='privacy_policy'),
 
     path('termini/', termini_utilizzo, name='termini_utilizzo'),
-
-    path(
-        'carica-workflow-professionali/<str:codice>/',
-        carica_workflow_professionali,
-        name='carica_workflow_professionali'
-    ),
-
-    path(
-        'pulisci-workflow-doppi/<str:codice>/',
-        pulisci_workflow_doppi,
-        name='pulisci_workflow_doppi'
-    ),
 ]
