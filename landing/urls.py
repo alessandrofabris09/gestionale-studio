7a51dd15f7b6fcad17324742b2c38c9e26a66523
003 Disegno <Dis003@stageo4.local>
2026-06-17 09:24:28 +0200

    Carica automaticamente tipi pratica iniziali

--- landing/urls.py
+++ landing/urls.py
@@ -4,6 +4,7 @@ from .views import (
     home,
     privacy_policy,
     termini_utilizzo,
+    carica_tipi_pratica,
 )
 
 
@@ -13,4 +14,10 @@ urlpatterns = [
     path('privacy/', privacy_policy, name='privacy_policy'),
 
     path('termini/', termini_utilizzo, name='termini_utilizzo'),
+
+    path(
+        'carica-tipi-pratica/<str:codice>/',
+        carica_tipi_pratica,
+        name='carica_tipi_pratica'
+    ),
 ]
