a2c37e1b63713aff74f357b0fca6e51d57938c3d
003 Disegno <Dis003@stageo4.local>
2026-06-17 09:32:10 +0200

    Rimuove caricamento temporaneo tipi pratica

--- landing/urls.py
+++ landing/urls.py
@@ -4,7 +4,6 @@ from .views import (
     home,
     privacy_policy,
     termini_utilizzo,
-    carica_tipi_pratica,
 )
 
 
@@ -14,10 +13,4 @@ urlpatterns = [
     path('privacy/', privacy_policy, name='privacy_policy'),
 
     path('termini/', termini_utilizzo, name='termini_utilizzo'),
-
-    path(
-        'carica-tipi-pratica/<str:codice>/',
-        carica_tipi_pratica,
-        name='carica_tipi_pratica'
-    ),
 ]
