65030bcbcf5ffdd6225876dad2ec4062417532d7
003 Disegno <Dis003@stageo4.local>
2026-06-17 09:16:34 +0200

    Rimuove ripristino temporaneo admin

--- landing/urls.py
+++ landing/urls.py
@@ -4,7 +4,6 @@ from .views import (
     home,
     privacy_policy,
     termini_utilizzo,
-    ripristina_admin,
 )
 
 
@@ -14,10 +13,4 @@ urlpatterns = [
     path('privacy/', privacy_policy, name='privacy_policy'),
 
     path('termini/', termini_utilizzo, name='termini_utilizzo'),
-
-    path(
-        'ripristina-admin/<str:codice>/',
-        ripristina_admin,
-        name='ripristina_admin'
-    ),
 ]
